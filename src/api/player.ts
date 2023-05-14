067aa7a99d9711b745be19d6752a2539f93c5b44
Mathias Bischoff <mathiasbischo@gmail.com>
2023-05-14 21:09:51 +0200

    -

--- src/api/player.ts
+++ src/api/player.ts
@@ -2,7 +2,6 @@ const API_KEY = import.meta.env.VITE_FACEIT_API_KEY;
 const BASE_PATH = import.meta.env.VITE_FACEIT_API_PATH
 const REQUEST_INIT: RequestInit = {
     method: 'GET',
-    mode: 'no-cors',
     headers: {
         'accept': 'application/json',
         'Authorization': `Bearer ${API_KEY}`,
@@ -17,7 +16,7 @@ enum EPlayerRoutes {
 export const getPlayerDetails = (nickname: string): Promise<any> => {
     console.log(BASE_PATH)
     return new Promise((resolve, reject) => fetch(`${BASE_PATH}${EPlayerRoutes.players}?nickname=${nickname}`, REQUEST_INIT)
-        .then((js) => js.json().then((res) => console.log(res)))
+        .then((js) => js.json().then((res) => resolve(res)))
         .catch((e) => reject(e))
     )
 }
